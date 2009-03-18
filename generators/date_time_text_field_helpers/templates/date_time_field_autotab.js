1e0c71f16d60ca84bddcebff79b8e21e509841c1
Michael Pope <michael@dtcorp.com.au>
2009-03-19 09:04:08 +1100

    Removed redundant keyAllow and fixed up keyRange.

--- generators/date_time_text_field_helpers/templates/date_time_field_autotab.js
+++ generators/date_time_text_field_helpers/templates/date_time_field_autotab.js
@@ -16,16 +16,14 @@ var AutoJumpToNextOnLength = Behavior.create({
   {
     this.inputLength = inputLength;
     this.element.setAttribute('autocomplete','off');
-    this.keyRange = $R(48, 57).toArray().concat($R(96, 105).toArray()); // all numberic characters
-
-    this.keyAllow = $R(48, 57).toArray();
+    this.keyRange = $R(48, 57).toArray(); // numberic characters
   }, 
 
   onkeydown: function(e)
   {
 
     // Stops extra characters being entered    
-      if (!this.keyAllow.include(e.keyCode)){
+      if (!this.keyRange.include(e.keyCode)){
 	  return false;
       }
 
